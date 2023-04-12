4cfdd7f2e29f9dbd971ef88476c744b6efffe9fa
Julie C <jucheng925@gmail.com>
2023-04-12 12:17:21 -0400

    cap each word of an array element

--- index.js
+++ index.js
@@ -11,6 +11,18 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+const titleCased = () => tutorials.map(capTitle)
+  
+function capTitle(title) {
+  let individualWords = title.split(" ")  //to separate out the individual words
+  const capWords = individualWords.map((words)=> words.charAt(0).toUpperCase() + words.slice(1)) 
+  let capTitles = ""
+  capWords.map((words)=> capTitles+= ` ${words}`)
+  return (capTitles.trimStart())
 }
+
+
+
+
+
+
